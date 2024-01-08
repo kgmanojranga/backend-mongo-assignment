567283490ec5a712cd367ec18a74755cdd42bca6
Manoj Ranga <manojranga7583@gmail.com>
2024-01-08 13:11:35 +0530

    refactor: remove unnecessary codes

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,10 @@
-import express, {NextFunction} from "express";
+import express from "express";
 import itemRoutes from "./routes/item-routes";
 
 const app = express()
 
 // json serialize
 app.use(express.json())
-
-// Custom middleware
-// app.use((req, res,next: NextFunction)=> {
-//     console.log( "Metarooooooooooooooon !!!!")
-//     next();
-// })
 
 // routes
 app.use('/api/v1/items', itemRoutes)
